374872708d6df2fc9dd633c582200c5e0b957c13
frank <you@example.com>
2018-07-03 11:00:28 +0200

    Update post list page

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ def post_list(request, template='blog/post_list.html'):
     context = {
         'page': {
             'title': 'JobUFO\'s Blog'
-        }
+        },
+        'author': 'Mighty Frank'
     }
     return render(request, template, context)
